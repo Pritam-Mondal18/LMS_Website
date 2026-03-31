3d297b94bcb93e1db1772bfc1976e8aff376519b
Pritam Mondal <pritammondal18012003@gmail.com>
2026-03-31 16:15:50 +0530

    enhance Edit Profile UI and backed logic

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,11 +1,11 @@
 import express from "express"; 
 import isAuth from "../middleware/isAuth.js";
-import {getCurrentUser} from "../controller/userControler.js";
+import upload from "../middleware/multer.js";
+import {getCurrentUser, updateProfile} from "../controller/userControler.js";
 
 const userRoute = express.Router();
 
 
 userRoute.get("/getcurrentuser", isAuth, getCurrentUser);
-
-
+userRoute.post ("/profile", isAuth,upload.single("photoUrl"),updateProfile)
 export default userRoute;
