1a2ca9a074de3fc968a447231378783d60a27591
Pritam Mondal <pritammondal18012003@gmail.com>
2026-04-02 14:53:34 +0530

    Fix the refreash problem and profile picture

--- frontend/src/component/Nav.jsx
+++ frontend/src/component/Nav.jsx
@@ -25,7 +25,7 @@ function Nav() {
             dispatch(setUserData(null))
             console.log(result.data)
             toast.success("Logout successful")
-        }catch{
+        }catch(error){
             console.log("error while logout")
             toast.error(error.response.data.message)   
         }
@@ -38,8 +38,8 @@ function Nav() {
                 </div>
                 <div className="w-[30%] lg:flex items-center justify-center gap-4 hidden">
 
-                    {!userData &&<IoPersonCircle className="w-[50px] h-[50px] text-white cursor-pointer" onClick={()=>setShow(prev=>!prev)}/>}
-                    {userData && <div className="w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-black border-white cursor-pointer" onClick={()=>setShow(prev=>!prev)}>
+                    {!userData && <IoPersonCircle className="w-[50px] h-[50px] text-white cursor-pointer" onClick={()=>setShow(prev=>!prev)}/>}
+                    {userData && userData?.photoUrl ? <img src={userData?.photoUrl} alt="Profile" className="w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-black border-white cursor-pointer" onClick={()=>setShow(prev=>!prev)}/>: <div className="w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-black border-white cursor-pointer" onClick={()=>setShow(prev=>!prev)}>
                         {userData?.name?.slice(0,1)?.toUpperCase()}
                         {/* {userData?.name ? userData.name[0].toUpperCase() : "?"} */}
                         {/* {userData?.name?.[0]?.toUpperCase() || "?"} */}
@@ -57,7 +57,7 @@ function Nav() {
                 <div className={`fixed top-0 left-0 w-[100vw] h-[100vh] bg-[#000000d6] flex items-center justify-center flex-col gap-5 z-10 lg:hidden ${showHam ? "translate-x-[0] transition duration-600" : "translate-x-[-100%] transition duration-600"}`}>
                     <ImCross className="w-[35px] h-[35px] fill-white absolute top-5 right-[4%] cursor-pointer" onClick={()=>setShowHam(prev=>!prev)}/>
                         {!userData &&<IoPersonCircle className="w-[50px] h-[50px] text-white cursor-pointer" />}
-                        {userData && <div className="w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-black border-white cursor-pointer" >
+                        {userData?.photoUrl ?<img src={userData?.photoUrl} alt="Profile" className="w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-black border-white cursor-pointer"/>:  <div className="w-[50px] h-[50px] rounded-full text-white flex items-center justify-center text-[20px] border-2 bg-black border-white cursor-pointer" >
                         {userData?.name?.slice(0,1)?.toUpperCase()}
                     </div>}
                     <div className="w-[200px] h-[65px] flex items-center justify-center border-2 border-white text-white bg-[black] rounded-[10px] text-[18px] font-light cursor-pointer" onClick={()=>navigate("/profile")}>My Profile</div>
--- frontend/src/customHooks/getCurrentUser.js
+++ frontend/src/customHooks/getCurrentUser.js
@@ -11,7 +11,7 @@ const getCurrentUser = () => {
     const fetchUser = async () => {
         try{
             const result = await axios.get(serverUrl + "/api/user/getcurrentuser", {withCredentials: true})
-            dispatch(setUserData(result.data))
+            dispatch(setUserData(result.data.user))
         }catch (error) {
             console.log(error)
             dispatch(setUserData(null))
