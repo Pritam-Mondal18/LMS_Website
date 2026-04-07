5a029f97459ff751c590d1bce30e12e9a449cf02
Pritam Mondal <pritammondal18012003@gmail.com>
2026-04-07 13:43:58 +0530

    intigrate backend with fronted of editCourse Section

--- frontend/src/pages/Educator/EditCourse.jsx
+++ frontend/src/pages/Educator/EditCourse.jsx
@@ -1,13 +1,86 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {FaArrowLeftLong} from 'react-icons/fa6';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import empty from '../../assets/empty.jpg';
 import {FaEdit} from 'react-icons/fa'
+import {serverUrl} from '../../App'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { ClipLoader } from 'react-spinners';
 
 function EditCourse() {
     const navigate = useNavigate();
+    const {courseId} = useParams()
     const thumb = useRef()
     const [isPublished, setIsPublished] = useState(false);
+    const [selectCourse,SetSelectCourse]=useState(null)
+    const [title,setTitle] = useState("")
+    const [subTitle,setSubTitle] = useState("")
+    const [description,setDescription] = useState("")
+    const [category,setCategory] = useState("")
+    const [level,setLevel] = useState("")
+    const [price,setPrice] = useState("")
+    const [frontendImage,setFrontendImage] = useState(empty)
+    const [backendImage,setBackendImage] = useState(null)
+    const [loading,setLoading] = useState(false)
+
+    const handleThumbnail = (e)=>{
+        const file = e.target.files[0]
+        setBackendImage(file)
+        setFrontendImage(URL.createObjectURL(file))
+    }
+
+    const getCourseById = async () => {
+        try{
+            const result = await axios.get(serverUrl+`/api/course/getcourse/${courseId}`,{withCredentials:true})
+            SetSelectCourse(result.data.course)
+            console.log(result.data)
+        }catch(error){
+            console.log(error)
+
+        }
+    }
+    useEffect(()=>{
+        if(selectCourse){
+            setTitle(selectCourse.title || "")
+            setSubTitle(selectCourse.subTitle || "")
+            setDescription(selectCourse.description || "")
+            setCategory(selectCourse.category || "")
+            setLevel(selectCourse.level || "")
+            setPrice(selectCourse.price || "")
+            setFrontendImage(selectCourse.thumbnail || empty)
+            setIsPublished(selectCourse?.isPublished)
+
+        }
+    },[selectCourse])
+    useEffect(()=>{
+        getCourseById()
+    },[])
+
+    const handleEditCourse = async () => {
+        setLoading(true)
+        const formData = new FormData()
+        formData.append("title",title)
+        formData.append("subTitle",subTitle)
+        formData.append("description",description)
+        formData.append("category",category)
+        formData.append("level",level)
+        formData.append("price",price)
+        formData.append("thumbnail",backendImage)
+        formData.append("isPublished",isPublished)
+        try{
+            const result = await axios.post(serverUrl+`/api/course/editcourse/${courseId}`,formData,{withCredentials:true})
+            console.log(result.data)
+            setLoading(false)
+            navigate("/courses")
+            toast.success("Course Updated")
+        }catch(error){
+            console.log(error)
+            toast.error(error.response.data.message)
+            setLoading(false)
+        }
+    }
+
     return(
         <div className='max-w-5xl mx-auto p-6 mt-10 bg-white rounded-lg shadow-md'>
             {/* top bar */}
@@ -26,24 +99,24 @@ function EditCourse() {
                 <form className='space-y-6' onSubmit={(e)=>e.preventDefault()}>
                     <div>
                         <label htmlFor="title" className='block text-sm font-medium text-gray-700 mb-1'>Title</label>
-                        <input id='title' type="text" placeholder='Course Title' className='w-full border px-4 py-2 rounded-md' />
+                        <input id='title' type="text" placeholder='Course Title' className='w-full border px-4 py-2 rounded-md' onChange={(e)=>setTitle(e.target.value)} value={title}/>
                     </div>
 
                      <div>
                         <label htmlFor="subtitle" className='block text-sm font-medium text-gray-700 mb-1'>SubTitle</label>
-                        <input id='subtitle' type="text" placeholder='Course SubTitle' className='w-full border px-4 py-2 rounded-md' />
+                        <input id='subtitle' type="text" placeholder='Course SubTitle' className='w-full border px-4 py-2 rounded-md' onChange={(e)=>setSubTitle(e.target.value)} value={subTitle}/>
                     </div>
 
                      <div>
                         <label htmlFor="description" className='block text-sm font-medium text-gray-700 mb-1'>Description</label>
-                        <textarea id='description' placeholder='Course Description' className='w-full border px-4 py-2 rounded-md h-24 resize-none' />
+                        <textarea id='description' placeholder='Course Description' className='w-full border px-4 py-2 rounded-md h-24 resize-none' onChange={(e)=>setDescription(e.target.value)} value={description}/>
                     </div>
 
                     <div className='flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0'>
                         {/* for category  */}
                         <div className='flex-1'>
                             <label htmlFor="" className='block text-sm font-medium text-gray-700 mb-1'>course Category</label>
-                            <select name="" id="" className='w-full border px-4 py-2 rounded-md bg-white'>
+                            <select name="" id="" className='w-full border px-4 py-2 rounded-md bg-white' onChange={(e)=>setCategory(e.target.value)} value={category}>
                                 <option value="">Select Category</option>
                                 <option value="Math">Math</option>
                                 <option value="Physics">Physics</option>
@@ -57,7 +130,7 @@ function EditCourse() {
                         {/* for level */}
                         <div className='flex-1'>
                             <label htmlFor="" className='block text-sm font-medium text-gray-700 mb-1'>course Level</label>
-                            <select name="" id="" className='w-full border px-4 py-2 rounded-md bg-white'>
+                            <select name="" id="" className='w-full border px-4 py-2 rounded-md bg-white' onChange={(e)=>setLevel(e.target.value)} value={level}>
                                 <option value="">Select Level</option>
                                 <option value="Beginner">Beginner</option>
                                 <option value="Intermediate">Intermediate</option>
@@ -67,27 +140,25 @@ function EditCourse() {
                         {/* for price */}
                         <div className='flex-1'>
                             <label htmlFor="price" className='block text-sm font-medium text-gray-700 mb-1'>course Price(INR)</label>
-                            <input type="number" id="price" className='w-full border px-4 py-2 rounded-md' placeholder='₹'/>
+                            <input type="number" id="price" className='w-full border px-4 py-2 rounded-md' placeholder='₹'onChange={(e)=>setPrice(e.target.value)} value={price}/>
                         </div>
                         
                         
                     </div>
                     <div>
                         <label htmlFor="thumbnail" className='block text-sm font-medium text-gray-700 mb-1'>Course Thumbnail</label>
-                        <input type="file" id="thumbnail" hidden ref={thumb} accept='image/*'/>
+                        <input type="file" id="thumbnail" hidden ref={thumb} accept='image/*' onChange={handleThumbnail}/>
                     </div>
                     <div className='relative w-[300px] h-[170px]'>
-                        <img src={empty} alt='' className='w-[100%] h-[100%] border-black rounded-[5px] cursor-pointer' onClick={()=>thumb.current.click()}/>
+                        <img src={frontendImage} alt='' className='w-[100%] h-[100%] border-black rounded-[5px] cursor-pointer' onClick={()=>thumb.current.click()}/>
                         <FaEdit className='w-[20px] h-[20px] absolute top-2 right-2' onClick={()=>thumb.current.click()}/>
                     </div>
 
                     <div className='flex items-center justify-start gap-[15px]'>
-                        <button className='bg-[#3e3e3e] hover:bg-green-600 text-white border-1 border-balck cursor-pointer px-7 py-2 rounded-md' onClick={()=>navigate("/courses")}>Save</button>
+                        <button className='bg-[#3e3e3e] hover:bg-green-600 text-white border-1 border-balck cursor-pointer px-7 py-2 rounded-md' onClick={handleEditCourse}>{loading?<ClipLoader/>:"Save"}</button>
                         <button className='bg-[#e9e8e8] hover:bg-red-300 text-black border-1 border-balck cursor-pointer px-7 py-2 rounded-md' onClick={()=>navigate("/courses")}>Cancel</button>
                         
                     </div>
-
-
                 </form>
             </div>
             </div>
